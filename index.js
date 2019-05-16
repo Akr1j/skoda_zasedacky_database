e5c4293a76abefd52d6923f93e18248d430ee631
Akr1j <jirkajandourek@seznam.cz>
2019-05-16 09:23:07 +0200

    JSON comunication
    Database comunication updated

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //Implementace knihovny express
 const express = require('express')
 const bodyParser = require("body-parser")
-
 const app = express()
 const apiRoute = express.Route()
+
 //Nastavení portu pro express
 const port = 3000
 
- console.log("in")
+
 //Implementace knihovny mysql
 var mysql = require('mysql');
 
@@ -20,25 +20,41 @@ var con = mysql.createConnection({
   database: "room_database",
 });
 
+
+
 //Připojení k databázi
 con.connect(function(err){
   if (err) throw err;
 });
-//app.use('/api', apiRoute);
+
+//Použití knihovny na převodu z JSON do objektu
 app.use(bodyParser.json())
 
-//Expres komunikace s klientem
+//Express komunikace s klientem
 
 //ROOM INFO
 //Nastavení poslechu na adresu a výsledek
 app.post('/api/roomData', function(req, res) {
-  console.log(req.body)
-  //throw err;
   //pokus o vytáhnutí id z ***
   var databaseId = req.body.id;
   //request na databázi
   //Objekt
-  requestInfoRoom(databaseId, (rawData) => {
+  requestInfoRoom(databaseId, function(rawData){
+    var myJSON = JSON.stringify(rawData[0]);
+    //poslání JSONu
+    res.send(myJSON);
+  });
+});
+
+//ROOM SCHEDULE
+//Nastavení poslechu na adresu a výsledek
+app.post('/api/roomSchedule ', function(req, res) {
+  //pokus o vytáhnutí id z ***
+  var databaseId = req.body.id;
+  var databaseDate = req.body.date;
+  //request na databázi
+  //Objekt
+  requestO(databaseId, databaseDate, function(rawData){
     var myJSON = JSON.stringify(rawData[0]);
     //poslání JSONu
     res.send(myJSON);
@@ -50,12 +66,39 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 
 
-
-
-
-
-
 //Komunikace s databází funkce
+
+//Vypíše informace o místnosti s konkrétním id
+function requestInfoRoom(data, callback){
+  con.query("SELECT * FROM rooms WHERE id = '" + con.escape(data) + "'", function (err, result, fields) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
+//Vypíše všechny události(název, od kdy, do kdy, kdo pořádá) na místnost s konkrétním id a konkrétním dnem
+function requestOccupiedTime(dataID, dataDATE, callback){
+  new RegExp('dddd-dd-dd');
+  con.query("SELECT occupied_from, occupied_to, name, description FROM occupied WHERE id = " + con.escape(dataID) + " AND occupied_date = " + con.escape(dataDATE), function (err, result, fields) {
+    if (err) throw err;
+    callback(result);
+  });
+}
+
+
+
+//DUMP
+
+
+/*
+//Vypíše informace o uživately s konkrétním id
+function requestInfoUser(data){
+  con.query("SELECT * FROM users WHERE id = '" + con.escape(data) + "'", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+}
+*/
 
 /*
 //Vypíše list id místnosní
@@ -67,30 +110,3 @@ function requestListIdRooms(){
   });
 }
 */
-
-//Vypíše informace o místnosti s konkrétním id
-function requestInfoRoom(data, callback){
-  con.query("SELECT * FROM rooms WHERE id = '" + con.escape(data) + "'", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result[0]);
-    callback(result);
-  });
-}
-
-//Vypíše informace o uživately s konkrétním id
-function requestInfoUser(data){
-  con.query("SELECT * FROM users WHERE id = '" + con.escape(data) + "'", function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-}
-
-//Vypíše všechny události(název, od kdy, do kdy, kdo pořádá) na místnost s konkrétním id a konkrétním dnem
-function requestOccupiedTime(dataID, dataDATE){
-  new RegExp('dddd-dd-dd');
-  con.query("SELECT occupied_from, occupied_to, name, description FROM occupied WHERE id = " + con.escape(dataID) + " AND occupied_date = " + con.escape(dataDATE), function (err, result, fields) {
-    if (err) throw err;
-    console.log(result);
-  });
-}
-
