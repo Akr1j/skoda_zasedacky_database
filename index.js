7b0165e78677d5cb663c80c8eda9e536664e75c0
Akr1j <jirkajandourek@seznam.cz>
2019-05-16 13:22:14 +0200

    Comunication with database
    Creat newRequestOccupationTime which add reservation room in database

--- index.js
+++ index.js
@@ -61,6 +61,34 @@ app.post('/api/roomSchedule ', function(req, res) {
   });
 });
 
+//NEW ROOM SCHEDULE
+//Nastavení poslechu na adresu a výsledek
+app.post('/api/addRoomReservation ', function(req, res) {
+  //pokus o vytáhnutí id z ***
+  /*
+  var databaseUserName = req.body.name;
+  var databaseDate = req.body.date;
+  var databaseFrom = req.body.occupiedFrom;
+  var databaseTo = req.body.occupiedTo;
+  var databaseName = req.body.name;
+  var databaseDescription = req.body.description;*/
+
+  var databaseUserName = "Jiří Testík";
+  var databaseDate = "2019-05-16";
+  var databaseFrom = "12:00:00";
+  var databaseTo = "13:00:00";
+  var databaseName = "Testování místností";
+  var databaseDescription = "Tato rezervace je zde pro účely testu";
+
+  //request na databázi
+  //Objekt
+  requestO(databaseId, databaseDate, function(rawData){
+    var myJSON = JSON.stringify(rawData[0]);
+    //poslání JSONu
+    res.send(myJSON);
+  });
+});
+
 //Zde čeká na příchod na PORT a pošle SENT (Nechává zapnutý průchod)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
@@ -69,21 +97,66 @@ app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 //Komunikace s databází funkce
 
 //Vypíše informace o místnosti s konkrétním id
-function requestInfoRoom(data, callback){
-  con.query("SELECT * FROM rooms WHERE id = '" + con.escape(data) + "'", function (err, result, fields) {
+function requestInfoRoom(dataId, callback){
+  con.query("SELECT * FROM rooms WHERE id = '" + con.escape(dataId) + "'", function (err, result, fields) {
     if (err) throw err;
     callback(result);
   });
 }
 
 //Vypíše všechny události(název, od kdy, do kdy, kdo pořádá) na místnost s konkrétním id a konkrétním dnem
-function requestOccupiedTime(dataID, dataDATE, callback){
+function requestOccupiedTime(dataId, dataDate, callback){
   new RegExp('dddd-dd-dd');
-  con.query("SELECT occupied_from, occupied_to, name, description FROM occupied WHERE id = " + con.escape(dataID) + " AND occupied_date = " + con.escape(dataDATE), function (err, result, fields) {
+  con.query("SELECT occupied_from, occupied_to, name, description FROM occupied WHERE id = " + con.escape(dataId) + " AND occupied_date = " + con.escape(dataDate), function (err, result, fields) {
     if (err) throw err;
     callback(result);
   });
 }
+
+//Tvorba nové rezervace
+function newRequestOccupiedTime(dataId, dataUserName, dataDate, dataFrom, dataTo, dataName, dataDescription, callback){
+  if (con.query("SELECT name FROM occupied WHERE (occupied_date ="+ dataDate +") AND (" +  dataFrom +" BETWEEN occupied_from AND occupied_to) AND (" + dataTo + "BETWEEN occupied_from AND occupied_to)",
+   function(err,result,fields) {
+    console.log("xxx",result)
+    con.query("INSERT INTO occupied VALUES ("+ dataId + ", " + dataName + ", " + dataDate +", " + dataFrom +", " + dataTo +", " + dataUserName +", " + dataDescription +")",
+     function(err,result,fields){
+      if (err) throw err;
+      callback(result);
+    });   
+  }));
+}
+
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
